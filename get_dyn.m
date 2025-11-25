e1eb81184667bf863c2c397604d0de2bc82119d9
zippyskippy3 <savannahkipp7@gmail.com>
2025-11-25 13:59:00 -0600

    get_dyn

---
+++ get_dyn.m
@@ -0,0 +1,17 @@
+robot_urdf_file = '/Users/savannahkipperman/Documents/KinematicSnake/KinematicSnakeControl/snake_robot/snake_description/urdf/threeJoints.urdf'; 
+gravity_vector = [0; 0; -9.81];
+
+robot = importrobot(robot_urdf_file);
+show(robot,Visuals="on",Collisions="off");
+
+robot.DataFormat = 'column';
+
+q = randomConfiguration(robot);
+
+q_dot = zeros(size(q));
+
+M_matrix = massMatrix(robot, q);
+
+G = gravityTorque(robot, q);
+
+C = velocityProduct(robot, q, q_dot);
