744572dbded1dbdb9c844b0aa0cc8472255408e1
zippyskippy3 <savannahkipp7@gmail.com>
2025-11-25 14:14:37 -0600

    push 2

--- get_dyn.m
+++ get_dyn.m
@@ -1,8 +1,8 @@
 robot_urdf_file = './snake_robot/snake_description/urdf/threeJoints.urdf'; 
 gravity_vector = [0; 0; -9.81];
 
-robot = importrobot(robot_urdf_file);
-show(robot,Visuals="on",Collisions="off");
+robot = importrobot(robot_urdf_file, MeshPath="./snake_robot/snake_description/meshes");
+show(robot,Visuals="on",Collisions="off", Frames="off");
 
 robot.DataFormat = 'column';
 
